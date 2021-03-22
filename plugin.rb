1ddbd7d6feab9ee5743c6ad59583020bae98a1d9
Tom Manion <thomaswmanion@gmail.com>
2021-03-22 10:43:30 -0500

    use register method

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,11 @@
 # +1.0/0.0 is Infinity
 infinity = +1.0/0.0
-GlobalSetting.max_reqs_per_ip_mode = "none"
-GlobalSetting.max_reqs_rate_limit_on_private = infinity
-GlobalSetting.max_admin_api_reqs_per_key_per_minute = infinity
-GlobalSetting.max_user_api_reqs_per_minute = infinity
-GlobalSetting.max_user_api_reqs_per_day = infinity
-GlobalSetting.max_reqs_per_ip_per_minute = infinity
-GlobalSetting.max_reqs_per_ip_per_10_seconds = infinity
-GlobalSetting.max_asset_reqs_per_ip_per_10_seconds = infinity
+GlobalSetting.register("max_reqs_per_ip_mode", "none")
+GlobalSetting.register("max_reqs_rate_limit_on_private", infinity)
+GlobalSetting.register("max_admin_api_reqs_per_key_per_minute", infinity)
+GlobalSetting.register("max_user_api_reqs_per_minute", infinity)
+GlobalSetting.register("max_user_api_reqs_per_day", infinity)
+GlobalSetting.register("max_reqs_per_ip_per_minute", infinity)
+GlobalSetting.register("max_reqs_per_ip_per_10_seconds", infinity)
+GlobalSetting.register("max_asset_reqs_per_ip_per_10_seconds", infinity)
 puts "API Rate Limit variables set to infinity"
